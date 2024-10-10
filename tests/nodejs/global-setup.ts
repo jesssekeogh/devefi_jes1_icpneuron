75e6ef7d0baf70253c69272900a23643472a2382
jesssekeogh <jesse.keogh74@gmail.com>
2024-10-10 23:53:31 +0100

    setting up picjs tests with nns state

--- tests/nodejs/global-setup.ts
+++
@@ -1,9 +0,0 @@
-import { PocketIcServer } from '@hadronous/pic';
-
-module.exports = async function (): Promise<void> {
-  const pic = await PocketIcServer.start();
-  const url = pic.getUrl();
-
-  process.env.PIC_URL = url;
-  global.__PIC__ = pic;
-};
