93636521185a905c38c67c70c225d9c620c42336
jesssekeogh <jesse.keogh74@gmail.com>
2024-10-14 18:31:30 +0100

    setting up stake neuron tests and latest devefi framework

--- tests/pic/global-setup.ts
+++ tests/pic/global-setup.ts
@@ -2,7 +2,7 @@ import { PocketIcServer } from '@hadronous/pic';
 
 module.exports = async function (): Promise<void> {
   const pic = await PocketIcServer.start({
-    showCanisterLogs: true,
+    // showCanisterLogs: true,
   });
   const url = pic.getUrl();
 
