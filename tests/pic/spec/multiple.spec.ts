ef43f7d10a8a10989e70f630f11f013783fe84c8
jesssekeogh <jesse.keogh74@gmail.com>
2024-10-29 14:24:27 +0000

    cleaning up pocketIC tests

--- tests/pic/spec/multiple.spec.ts
+++ tests/pic/spec/multiple.spec.ts
@@ -1,36 +1,32 @@
 import { Manager } from "../setup/manager.ts";
 import { NodeShared } from "../declarations/nnsvector/nnsvector.did.js";
 import { Maturity } from "../setup/maturity.ts";
-import { createIdentity } from "@hadronous/pic";
-import { Setup } from "../setup/setup.ts";
+import {
+  AMOUNT_TO_STAKE,
+  EXPECTED_TRANSACTION_FEES,
+  MINIMUM_DISSOLVE_DELAY,
+  MOCK_FOLLOWEE_TO_SET,
+} from "../setup/constants.ts";
 
 describe("Multiple", () => {
-  let setup: Setup;
   let manager: Manager;
   let maturity: Maturity;
   let nodes: NodeShared[];
-  let amountToStake: bigint = 10_0000_0000n;
-  let expectedTransactionFees: bigint = 20_000n;
-  let dissolveDelayToSet: bigint = 15897600n; // 184 days
-  let followeeToSet: bigint = 6914974521667616512n;
-  let isDissolving: boolean = false;
-  let followeeNeuronId: bigint;
+  let maturityFollowee: bigint;
 
   beforeAll(async () => {
-    let me = createIdentity("superSecretAlicePassword");
-    setup = await Setup.beforeAll();
-    manager = await Manager.beforeAll(setup.getPicInstance(), me);
+    manager = await Manager.beforeAll();
     maturity = Maturity.beforeAll(manager);
-    followeeNeuronId = await maturity.createNeuron();
+    maturityFollowee = await maturity.createNeuron();
 
     const nodesToCreate = 3;
 
     let done = [];
     for (let i = 0; i < nodesToCreate; i++) {
-      let node = await manager.stakeNeuron(amountToStake, {
-        dissolveDelay: dissolveDelayToSet,
-        followee: followeeToSet,
-        dissolving: isDissolving,
+      let node = await manager.stakeNeuron(AMOUNT_TO_STAKE, {
+        dissolveDelay: MINIMUM_DISSOLVE_DELAY,
+        followee: MOCK_FOLLOWEE_TO_SET,
+        dissolving: false,
       });
       done.push(node);
     }
@@ -39,21 +35,21 @@ describe("Multiple", () => {
   });
 
   afterAll(async () => {
-    await setup.afterAll();
+    await manager.afterAll();
   });
 
   it("should stake multiple neurons", async () => {
     for (let node of nodes) {
       expect(node.custom.nns_neuron.cache.neuron_id[0]).toBeDefined();
       expect(node.custom.nns_neuron.cache.cached_neuron_stake_e8s[0]).toBe(
-        amountToStake - expectedTransactionFees
+        AMOUNT_TO_STAKE - EXPECTED_TRANSACTION_FEES
       );
     }
   });
 
   it("should update multiple neurons", async () => {
     for (let node of nodes) {
-      await manager.modifyNode(node.id, [], [followeeNeuronId], []);
+      await manager.modifyNode(node.id, [], [maturityFollowee], []);
       await manager.advanceBlocksAndTime(3);
     }
 
@@ -62,25 +58,25 @@ describe("Multiple", () => {
     for (let node of nodes) {
       node = await manager.getNode(node.id);
       expect(node.custom.nns_neuron.variables.update_followee).toBe(
-        followeeNeuronId
+        maturityFollowee
       );
       expect(node.custom.nns_neuron.cache.followees).toHaveLength(3);
 
       for (let followee of node.custom.nns_neuron.cache.followees) {
-        expect(followee[1].followees[0].id).toBe(followeeNeuronId);
+        expect(followee[1].followees[0].id).toBe(maturityFollowee);
       }
     }
   });
 
   it("should increase multiple neurons stake", async () => {
-    let currentStake = amountToStake - expectedTransactionFees;
+    let currentStake = AMOUNT_TO_STAKE - EXPECTED_TRANSACTION_FEES;
     let sends = 3n;
 
     for (let node of nodes) {
       for (let i = 0n; i < sends; i++) {
         await manager.sendIcp(
           manager.getNodeSourceAccount(node),
-          amountToStake
+          AMOUNT_TO_STAKE
         );
         await manager.advanceBlocksAndTime(1);
       }
@@ -92,13 +88,13 @@ describe("Multiple", () => {
       node = await manager.getNode(node.id);
       expect(node.custom.nns_neuron.internals.refresh_idx).toHaveLength(0);
       expect(node.custom.nns_neuron.cache.cached_neuron_stake_e8s[0]).toBe(
-        currentStake + (amountToStake - expectedTransactionFees) * sends
+        currentStake + (AMOUNT_TO_STAKE - EXPECTED_TRANSACTION_FEES) * sends
       );
     }
   });
 
   it("should spawn maturity in multiple neurons", async () => {
-    await maturity.createMotionProposal(followeeNeuronId);
+    await maturity.createMotionProposal(maturityFollowee);
     await manager.advanceTime(20160); // 2 weeks
     await manager.advanceBlocks(10);
     await manager.advanceBlocksAndTime(3);
@@ -128,5 +124,4 @@ describe("Multiple", () => {
     let newBalance = await manager.getMyBalances();
     expect(newBalance.icp_tokens).toBeGreaterThan(oldBalance.icp_tokens);
   });
-  
 });
