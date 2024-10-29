54227f0129acaf41700bb9491868dc7c7fec5a74
jesssekeogh <jesse.keogh74@gmail.com>
2024-10-29 14:24:27 +0000

    cleaning up pocketIC tests

--- tests/pic/setup/constants.ts
+++ tests/pic/setup/constants.ts
@@ -19,3 +19,22 @@ export const NNS_STATE_PATH = resolve(
 
 export const NNS_SUBNET_ID =
   "5zbf7-w773a-srrlr-keamy-7pusd-nlghp-ml5pu-ivrne-y67rh-zkpyc-iqe";
+
+//
+// helper constants:
+//
+
+export const AMOUNT_TO_STAKE: bigint = 10_0000_0000n;
+
+export const EXPECTED_TRANSACTION_FEES: bigint = 20_000n;
+
+export const MINIMUM_DISSOLVE_DELAY: bigint = 15897600n;
+
+export const ONE_YEAR_SECONDS: bigint =
+  ((4n * 365n + 1n) * (24n * 60n * 60n)) / 4n;
+
+export const MAX_DISSOLVE_DELAY: bigint = 8n * ONE_YEAR_SECONDS;
+
+export const MOCK_FOLLOWEE_TO_SET: bigint = 6914974521667616512n;
+
+export const MOCK_FOLLOWEE_TO_SET_2: bigint = 8571487073262291504n;
