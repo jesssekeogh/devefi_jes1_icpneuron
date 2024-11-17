ea5cc6ca4d4b26853489f3d1eca7dc66442ea149
jesssekeogh <jesse.keogh74@gmail.com>
2024-11-17 14:03:37 +0000

    files needed for running tests

---
+++ tests/pic/declarations/nnsvector/nnsvector.did.d.ts
@@ -0,0 +1,369 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Account {
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
+export type Activity = {
+    'Ok' : { 'operation' : string, 'timestamp' : bigint }
+  } |
+  { 'Err' : { 'msg' : string, 'operation' : string, 'timestamp' : bigint } };
+export interface ArchivedTransactionResponse {
+  'args' : Array<TransactionRange>,
+  'callback' : GetTransactionsFn,
+}
+export interface BatchCommandRequest {
+  'request_id' : [] | [number],
+  'controller' : Controller,
+  'signature' : [] | [Uint8Array | number[]],
+  'expire_at' : [] | [bigint],
+  'commands' : Array<Command>,
+}
+export type BatchCommandResponse = {
+    'ok' : { 'id' : bigint, 'commands' : Array<CommandResponse> }
+  } |
+  {
+    'err' : { 'caller_not_controller' : null } |
+      { 'expired' : null } |
+      { 'other' : string } |
+      { 'duplicate' : bigint } |
+      { 'invalid_signature' : null }
+  };
+export interface Billing {
+  'transaction_fee' : BillingTransactionFee,
+  'cost_per_day' : bigint,
+}
+export interface BillingFeeSplit {
+  'platform' : bigint,
+  'author' : bigint,
+  'affiliate' : bigint,
+  'pylon' : bigint,
+}
+export interface BillingPylon {
+  'operation_cost' : bigint,
+  'freezing_threshold_days' : bigint,
+  'min_create_balance' : bigint,
+  'split' : BillingFeeSplit,
+  'ledger' : Principal,
+  'platform_account' : Account,
+  'pylon_account' : Account,
+}
+export type BillingTransactionFee = { 'none' : null } |
+  { 'transaction_percentage_fee_e8s' : bigint } |
+  { 'flat_fee_multiplier' : bigint };
+export interface BlockType { 'url' : string, 'block_type' : string }
+export type Command = { 'modify_node' : ModifyNodeRequest } |
+  { 'create_node' : CreateNodeRequest } |
+  { 'transfer' : TransferRequest } |
+  { 'delete_node' : LocalNodeId };
+export type CommandResponse = { 'modify_node' : ModifyNodeResponse } |
+  { 'create_node' : CreateNodeResponse } |
+  { 'transfer' : TransferResponse } |
+  { 'delete_node' : DeleteNodeResp };
+export interface CommonCreateRequest {
+  'controllers' : Array<Controller>,
+  'extractors' : Uint32Array | number[],
+  'temp_id' : number,
+  'billing_option' : bigint,
+  'destinations' : Array<[] | [InputAddress]>,
+  'sources' : Array<[] | [InputAddress]>,
+  'affiliate' : [] | [Account],
+  'ledgers' : Array<SupportedLedger>,
+  'temporary' : boolean,
+  'refund' : Account,
+}
+export interface CommonModifyRequest {
+  'active' : [] | [boolean],
+  'controllers' : [] | [Array<Controller>],
+  'extractors' : [] | [Uint32Array | number[]],
+  'destinations' : [] | [Array<[] | [InputAddress]>],
+  'sources' : [] | [Array<[] | [InputAddress]>],
+  'refund' : [] | [Account],
+}
+export interface Controller {
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
+export type CreateNodeRequest = [CommonCreateRequest, CreateRequest];
+export type CreateNodeResponse = { 'ok' : GetNodeResponse } |
+  { 'err' : string };
+export type CreateRequest = { 'devefi_jes1_icpneuron' : CreateRequest__1 };
+export interface CreateRequest__1 {
+  'variables' : {
+    'dissolve_delay' : DissolveDelay,
+    'dissolve_status' : DissolveStatus,
+    'followee' : Followee,
+  },
+}
+export interface DataCertificate {
+  'certificate' : Uint8Array | number[],
+  'hash_tree' : Uint8Array | number[],
+}
+export type DeleteNodeResp = { 'ok' : null } |
+  { 'err' : string };
+export interface DestinationEndpointResp {
+  'endpoint' : EndpointOpt,
+  'name' : string,
+}
+export type DissolveDelay = { 'Default' : null } |
+  { 'DelayDays' : bigint };
+export type DissolveStatus = { 'Locked' : null } |
+  { 'Dissolving' : null };
+export type Endpoint = { 'ic' : EndpointIC } |
+  { 'other' : EndpointOther };
+export interface EndpointIC { 'ledger' : Principal, 'account' : Account }
+export type EndpointIdx = number;
+export type EndpointOpt = { 'ic' : EndpointOptIC } |
+  { 'other' : EndpointOptOther };
+export interface EndpointOptIC {
+  'ledger' : Principal,
+  'account' : [] | [Account],
+}
+export interface EndpointOptOther {
+  'platform' : bigint,
+  'ledger' : Uint8Array | number[],
+  'account' : [] | [Uint8Array | number[]],
+}
+export interface EndpointOther {
+  'platform' : bigint,
+  'ledger' : Uint8Array | number[],
+  'account' : Uint8Array | number[],
+}
+export type EndpointsDescription = Array<[LedgerIdx, LedgerLabel]>;
+export type Followee = { 'Default' : null } |
+  { 'FolloweeId' : bigint };
+export interface GetArchivesArgs { 'from' : [] | [Principal] }
+export type GetArchivesResult = Array<GetArchivesResultItem>;
+export interface GetArchivesResultItem {
+  'end' : bigint,
+  'canister_id' : Principal,
+  'start' : bigint,
+}
+export type GetBlocksArgs = Array<TransactionRange>;
+export interface GetBlocksResult {
+  'log_length' : bigint,
+  'blocks' : Array<{ 'id' : bigint, 'block' : [] | [Value] }>,
+  'archived_blocks' : Array<ArchivedTransactionResponse>,
+}
+export interface GetControllerNodesRequest {
+  'id' : Controller,
+  'start' : LocalNodeId,
+  'length' : number,
+}
+export type GetNode = { 'id' : LocalNodeId } |
+  { 'endpoint' : Endpoint };
+export interface GetNodeResponse {
+  'id' : LocalNodeId,
+  'created' : bigint,
+  'active' : boolean,
+  'modified' : bigint,
+  'controllers' : Array<Controller>,
+  'custom' : [] | [Shared],
+  'extractors' : Uint32Array | number[],
+  'billing' : {
+    'transaction_fee' : BillingTransactionFee,
+    'expires' : [] | [bigint],
+    'current_balance' : bigint,
+    'billing_option' : bigint,
+    'account' : Account,
+    'frozen' : boolean,
+    'cost_per_day' : bigint,
+  },
+  'destinations' : Array<DestinationEndpointResp>,
+  'sources' : Array<SourceEndpointResp>,
+  'refund' : Account,
+}
+export type GetTransactionsFn = ActorMethod<
+  [Array<TransactionRange>],
+  GetTransactionsResult
+>;
+export interface GetTransactionsResult {
+  'log_length' : bigint,
+  'blocks' : Array<{ 'id' : bigint, 'block' : [] | [Value] }>,
+  'archived_blocks' : Array<ArchivedTransactionResponse>,
+}
+export interface Info {
+  'pending' : bigint,
+  'last_indexed_tx' : bigint,
+  'errors' : bigint,
+  'lastTxTime' : bigint,
+  'accounts' : bigint,
+  'actor_principal' : [] | [Principal],
+  'reader_instructions_cost' : bigint,
+  'sender_instructions_cost' : bigint,
+}
+export interface Info__1 {
+  'pending' : bigint,
+  'last_indexed_tx' : bigint,
+  'errors' : bigint,
+  'lastTxTime' : bigint,
+  'accounts' : bigint,
+  'actor_principal' : Principal,
+  'reader_instructions_cost' : bigint,
+  'sender_instructions_cost' : bigint,
+}
+export type InputAddress = { 'ic' : Account } |
+  { 'other' : Uint8Array | number[] } |
+  { 'temp' : { 'id' : number, 'source_idx' : EndpointIdx } };
+export type LedgerIdx = bigint;
+export interface LedgerInfo {
+  'id' : Principal,
+  'info' : { 'icp' : Info } |
+    { 'icrc' : Info__1 },
+}
+export type LedgerLabel = string;
+export type LocalNodeId = number;
+export type ModifyNodeRequest = [
+  LocalNodeId,
+  [] | [CommonModifyRequest],
+  [] | [ModifyRequest],
+];
+export type ModifyNodeResponse = { 'ok' : GetNodeResponse } |
+  { 'err' : string };
+export type ModifyRequest = { 'devefi_jes1_icpneuron' : ModifyRequest__1 };
+export interface ModifyRequest__1 {
+  'dissolve_delay' : [] | [DissolveDelay],
+  'dissolve_status' : [] | [DissolveStatus],
+  'followee' : [] | [Followee],
+}
+export interface ModuleMeta {
+  'id' : string,
+  'create_allowed' : boolean,
+  'ledger_slots' : Array<string>,
+  'name' : string,
+  'billing' : Array<Billing>,
+  'description' : string,
+  'supported_ledgers' : Array<SupportedLedger>,
+  'author' : string,
+  'version' : Version,
+  'destinations' : EndpointsDescription,
+  'sources' : EndpointsDescription,
+  'temporary_allowed' : boolean,
+  'author_account' : Account,
+}
+export interface NNSVECTOR {
+  'get_ledger_errors' : ActorMethod<[], Array<Array<string>>>,
+  'get_ledgers_info' : ActorMethod<[], Array<LedgerInfo>>,
+  'icrc3_get_archives' : ActorMethod<[GetArchivesArgs], GetArchivesResult>,
+  'icrc3_get_blocks' : ActorMethod<[GetBlocksArgs], GetBlocksResult>,
+  'icrc3_get_tip_certificate' : ActorMethod<[], [] | [DataCertificate]>,
+  'icrc3_supported_block_types' : ActorMethod<[], Array<BlockType>>,
+  'icrc55_command' : ActorMethod<[BatchCommandRequest], BatchCommandResponse>,
+  'icrc55_get_controller_nodes' : ActorMethod<
+    [GetControllerNodesRequest],
+    Array<NodeShared>
+  >,
+  'icrc55_get_defaults' : ActorMethod<[string], CreateRequest>,
+  'icrc55_get_nodes' : ActorMethod<[Array<GetNode>], Array<[] | [NodeShared]>>,
+  'icrc55_get_pylon_meta' : ActorMethod<[], PylonMetaResp>,
+  'icrc55_virtual_balances' : ActorMethod<
+    [VirtualBalancesRequest],
+    VirtualBalancesResponse
+  >,
+}
+export interface NodeShared {
+  'id' : LocalNodeId,
+  'created' : bigint,
+  'active' : boolean,
+  'modified' : bigint,
+  'controllers' : Array<Controller>,
+  'custom' : [] | [Shared],
+  'extractors' : Uint32Array | number[],
+  'billing' : {
+    'transaction_fee' : BillingTransactionFee,
+    'expires' : [] | [bigint],
+    'current_balance' : bigint,
+    'billing_option' : bigint,
+    'account' : Account,
+    'frozen' : boolean,
+    'cost_per_day' : bigint,
+  },
+  'destinations' : Array<DestinationEndpointResp>,
+  'sources' : Array<SourceEndpointResp>,
+  'refund' : Account,
+}
+export interface PylonMetaResp {
+  'name' : string,
+  'billing' : BillingPylon,
+  'supported_ledgers' : Array<SupportedLedger>,
+  'request_max_expire_sec' : bigint,
+  'governed_by' : string,
+  'temporary_nodes' : { 'allowed' : boolean, 'expire_sec' : bigint },
+  'modules' : Array<ModuleMeta>,
+}
+export type Shared = { 'devefi_jes1_icpneuron' : Shared__1 };
+export interface SharedNeuronCache {
+  'dissolve_delay_seconds' : [] | [bigint],
+  'maturity_e8s_equivalent' : [] | [bigint],
+  'cached_neuron_stake_e8s' : [] | [bigint],
+  'created_timestamp_seconds' : [] | [bigint],
+  'state' : [] | [number],
+  'nonce' : [] | [bigint],
+  'followees' : Array<[number, { 'followees' : Array<{ 'id' : bigint }> }]>,
+  'voting_power' : [] | [bigint],
+  'neuron_id' : [] | [bigint],
+  'age_seconds' : [] | [bigint],
+}
+export interface Shared__1 {
+  'log' : Array<Activity>,
+  'internals' : {
+    'local_idx' : number,
+    'refresh_idx' : [] | [bigint],
+    'updating' : UpdatingStatus,
+    'spawning_neurons' : Array<SharedNeuronCache>,
+  },
+  'cache' : SharedNeuronCache,
+  'variables' : {
+    'dissolve_delay' : DissolveDelay,
+    'dissolve_status' : DissolveStatus,
+    'followee' : Followee,
+  },
+}
+export interface SourceEndpointResp {
+  'balance' : bigint,
+  'endpoint' : Endpoint,
+  'name' : string,
+}
+export type SupportedLedger = { 'ic' : Principal } |
+  { 'other' : { 'platform' : bigint, 'ledger' : Uint8Array | number[] } };
+export interface TransactionRange { 'start' : bigint, 'length' : bigint }
+export interface TransferRequest {
+  'to' : { 'node_billing' : LocalNodeId } |
+    { 'node' : { 'node_id' : LocalNodeId, 'endpoint_idx' : EndpointIdx } } |
+    {
+      'external_account' : { 'ic' : Account } |
+        { 'other' : Uint8Array | number[] }
+    } |
+    { 'account' : Account },
+  'from' : {
+      'node' : { 'node_id' : LocalNodeId, 'endpoint_idx' : EndpointIdx }
+    } |
+    { 'account' : Account },
+  'ledger' : SupportedLedger,
+  'amount' : bigint,
+}
+export type TransferResponse = { 'ok' : bigint } |
+  { 'err' : string };
+export type UpdatingStatus = { 'Calling' : bigint } |
+  { 'Done' : bigint } |
+  { 'Init' : null };
+export type Value = { 'Int' : bigint } |
+  { 'Map' : Array<ValueMap> } |
+  { 'Nat' : bigint } |
+  { 'Blob' : Uint8Array | number[] } |
+  { 'Text' : string } |
+  { 'Array' : Array<Value> };
+export type ValueMap = [string, Value];
+export type Version = { 'alpha' : Uint16Array | number[] } |
+  { 'beta' : Uint16Array | number[] } |
+  { 'release' : Uint16Array | number[] };
+export interface VirtualBalancesRequest {
+  'owner' : Principal,
+  'subaccount' : [] | [Uint8Array | number[]],
+}
+export type VirtualBalancesResponse = Array<[SupportedLedger, bigint]>;
+export interface _SERVICE extends NNSVECTOR {}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
---
+++ tests/pic/declarations/nnsvector/nnsvector.did.js
@@ -0,0 +1,462 @@
+export const idlFactory = ({ IDL }) => {
+  const ArchivedTransactionResponse = IDL.Rec();
+  const Value = IDL.Rec();
+  const Info = IDL.Record({
+    'pending' : IDL.Nat,
+    'last_indexed_tx' : IDL.Nat,
+    'errors' : IDL.Nat,
+    'lastTxTime' : IDL.Nat64,
+    'accounts' : IDL.Nat,
+    'actor_principal' : IDL.Opt(IDL.Principal),
+    'reader_instructions_cost' : IDL.Nat64,
+    'sender_instructions_cost' : IDL.Nat64,
+  });
+  const Info__1 = IDL.Record({
+    'pending' : IDL.Nat,
+    'last_indexed_tx' : IDL.Nat,
+    'errors' : IDL.Nat,
+    'lastTxTime' : IDL.Nat64,
+    'accounts' : IDL.Nat,
+    'actor_principal' : IDL.Principal,
+    'reader_instructions_cost' : IDL.Nat64,
+    'sender_instructions_cost' : IDL.Nat64,
+  });
+  const LedgerInfo = IDL.Record({
+    'id' : IDL.Principal,
+    'info' : IDL.Variant({ 'icp' : Info, 'icrc' : Info__1 }),
+  });
+  const GetArchivesArgs = IDL.Record({ 'from' : IDL.Opt(IDL.Principal) });
+  const GetArchivesResultItem = IDL.Record({
+    'end' : IDL.Nat,
+    'canister_id' : IDL.Principal,
+    'start' : IDL.Nat,
+  });
+  const GetArchivesResult = IDL.Vec(GetArchivesResultItem);
+  const TransactionRange = IDL.Record({
+    'start' : IDL.Nat,
+    'length' : IDL.Nat,
+  });
+  const GetBlocksArgs = IDL.Vec(TransactionRange);
+  const ValueMap = IDL.Tuple(IDL.Text, Value);
+  Value.fill(
+    IDL.Variant({
+      'Int' : IDL.Int,
+      'Map' : IDL.Vec(ValueMap),
+      'Nat' : IDL.Nat,
+      'Blob' : IDL.Vec(IDL.Nat8),
+      'Text' : IDL.Text,
+      'Array' : IDL.Vec(Value),
+    })
+  );
+  const GetTransactionsResult = IDL.Record({
+    'log_length' : IDL.Nat,
+    'blocks' : IDL.Vec(
+      IDL.Record({ 'id' : IDL.Nat, 'block' : IDL.Opt(Value) })
+    ),
+    'archived_blocks' : IDL.Vec(ArchivedTransactionResponse),
+  });
+  const GetTransactionsFn = IDL.Func(
+      [IDL.Vec(TransactionRange)],
+      [GetTransactionsResult],
+      ['query'],
+    );
+  ArchivedTransactionResponse.fill(
+    IDL.Record({
+      'args' : IDL.Vec(TransactionRange),
+      'callback' : GetTransactionsFn,
+    })
+  );
+  const GetBlocksResult = IDL.Record({
+    'log_length' : IDL.Nat,
+    'blocks' : IDL.Vec(
+      IDL.Record({ 'id' : IDL.Nat, 'block' : IDL.Opt(Value) })
+    ),
+    'archived_blocks' : IDL.Vec(ArchivedTransactionResponse),
+  });
+  const DataCertificate = IDL.Record({
+    'certificate' : IDL.Vec(IDL.Nat8),
+    'hash_tree' : IDL.Vec(IDL.Nat8),
+  });
+  const BlockType = IDL.Record({ 'url' : IDL.Text, 'block_type' : IDL.Text });
+  const Controller = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const LocalNodeId = IDL.Nat32;
+  const Account = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const EndpointIdx = IDL.Nat8;
+  const InputAddress = IDL.Variant({
+    'ic' : Account,
+    'other' : IDL.Vec(IDL.Nat8),
+    'temp' : IDL.Record({ 'id' : IDL.Nat32, 'source_idx' : EndpointIdx }),
+  });
+  const CommonModifyRequest = IDL.Record({
+    'active' : IDL.Opt(IDL.Bool),
+    'controllers' : IDL.Opt(IDL.Vec(Controller)),
+    'extractors' : IDL.Opt(IDL.Vec(LocalNodeId)),
+    'destinations' : IDL.Opt(IDL.Vec(IDL.Opt(InputAddress))),
+    'sources' : IDL.Opt(IDL.Vec(IDL.Opt(InputAddress))),
+    'refund' : IDL.Opt(Account),
+  });
+  const DissolveDelay = IDL.Variant({
+    'Default' : IDL.Null,
+    'DelayDays' : IDL.Nat64,
+  });
+  const DissolveStatus = IDL.Variant({
+    'Locked' : IDL.Null,
+    'Dissolving' : IDL.Null,
+  });
+  const Followee = IDL.Variant({
+    'Default' : IDL.Null,
+    'FolloweeId' : IDL.Nat64,
+  });
+  const ModifyRequest__1 = IDL.Record({
+    'dissolve_delay' : IDL.Opt(DissolveDelay),
+    'dissolve_status' : IDL.Opt(DissolveStatus),
+    'followee' : IDL.Opt(Followee),
+  });
+  const ModifyRequest = IDL.Variant({
+    'devefi_jes1_icpneuron' : ModifyRequest__1,
+  });
+  const ModifyNodeRequest = IDL.Tuple(
+    LocalNodeId,
+    IDL.Opt(CommonModifyRequest),
+    IDL.Opt(ModifyRequest),
+  );
+  const SupportedLedger = IDL.Variant({
+    'ic' : IDL.Principal,
+    'other' : IDL.Record({
+      'platform' : IDL.Nat64,
+      'ledger' : IDL.Vec(IDL.Nat8),
+    }),
+  });
+  const CommonCreateRequest = IDL.Record({
+    'controllers' : IDL.Vec(Controller),
+    'extractors' : IDL.Vec(LocalNodeId),
+    'temp_id' : IDL.Nat32,
+    'billing_option' : IDL.Nat,
+    'destinations' : IDL.Vec(IDL.Opt(InputAddress)),
+    'sources' : IDL.Vec(IDL.Opt(InputAddress)),
+    'affiliate' : IDL.Opt(Account),
+    'ledgers' : IDL.Vec(SupportedLedger),
+    'temporary' : IDL.Bool,
+    'refund' : Account,
+  });
+  const CreateRequest__1 = IDL.Record({
+    'variables' : IDL.Record({
+      'dissolve_delay' : DissolveDelay,
+      'dissolve_status' : DissolveStatus,
+      'followee' : Followee,
+    }),
+  });
+  const CreateRequest = IDL.Variant({
+    'devefi_jes1_icpneuron' : CreateRequest__1,
+  });
+  const CreateNodeRequest = IDL.Tuple(CommonCreateRequest, CreateRequest);
+  const TransferRequest = IDL.Record({
+    'to' : IDL.Variant({
+      'node_billing' : LocalNodeId,
+      'node' : IDL.Record({
+        'node_id' : LocalNodeId,
+        'endpoint_idx' : EndpointIdx,
+      }),
+      'external_account' : IDL.Variant({
+        'ic' : Account,
+        'other' : IDL.Vec(IDL.Nat8),
+      }),
+      'account' : Account,
+    }),
+    'from' : IDL.Variant({
+      'node' : IDL.Record({
+        'node_id' : LocalNodeId,
+        'endpoint_idx' : EndpointIdx,
+      }),
+      'account' : Account,
+    }),
+    'ledger' : SupportedLedger,
+    'amount' : IDL.Nat,
+  });
+  const Command = IDL.Variant({
+    'modify_node' : ModifyNodeRequest,
+    'create_node' : CreateNodeRequest,
+    'transfer' : TransferRequest,
+    'delete_node' : LocalNodeId,
+  });
+  const BatchCommandRequest = IDL.Record({
+    'request_id' : IDL.Opt(IDL.Nat32),
+    'controller' : Controller,
+    'signature' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'expire_at' : IDL.Opt(IDL.Nat64),
+    'commands' : IDL.Vec(Command),
+  });
+  const Activity = IDL.Variant({
+    'Ok' : IDL.Record({ 'operation' : IDL.Text, 'timestamp' : IDL.Nat64 }),
+    'Err' : IDL.Record({
+      'msg' : IDL.Text,
+      'operation' : IDL.Text,
+      'timestamp' : IDL.Nat64,
+    }),
+  });
+  const UpdatingStatus = IDL.Variant({
+    'Calling' : IDL.Nat64,
+    'Done' : IDL.Nat64,
+    'Init' : IDL.Null,
+  });
+  const SharedNeuronCache = IDL.Record({
+    'dissolve_delay_seconds' : IDL.Opt(IDL.Nat64),
+    'maturity_e8s_equivalent' : IDL.Opt(IDL.Nat64),
+    'cached_neuron_stake_e8s' : IDL.Opt(IDL.Nat64),
+    'created_timestamp_seconds' : IDL.Opt(IDL.Nat64),
+    'state' : IDL.Opt(IDL.Int32),
+    'nonce' : IDL.Opt(IDL.Nat64),
+    'followees' : IDL.Vec(
+      IDL.Tuple(
+        IDL.Int32,
+        IDL.Record({ 'followees' : IDL.Vec(IDL.Record({ 'id' : IDL.Nat64 })) }),
+      )
+    ),
+    'voting_power' : IDL.Opt(IDL.Nat64),
+    'neuron_id' : IDL.Opt(IDL.Nat64),
+    'age_seconds' : IDL.Opt(IDL.Nat64),
+  });
+  const Shared__1 = IDL.Record({
+    'log' : IDL.Vec(Activity),
+    'internals' : IDL.Record({
+      'local_idx' : IDL.Nat32,
+      'refresh_idx' : IDL.Opt(IDL.Nat64),
+      'updating' : UpdatingStatus,
+      'spawning_neurons' : IDL.Vec(SharedNeuronCache),
+    }),
+    'cache' : SharedNeuronCache,
+    'variables' : IDL.Record({
+      'dissolve_delay' : DissolveDelay,
+      'dissolve_status' : DissolveStatus,
+      'followee' : Followee,
+    }),
+  });
+  const Shared = IDL.Variant({ 'devefi_jes1_icpneuron' : Shared__1 });
+  const BillingTransactionFee = IDL.Variant({
+    'none' : IDL.Null,
+    'transaction_percentage_fee_e8s' : IDL.Nat,
+    'flat_fee_multiplier' : IDL.Nat,
+  });
+  const EndpointOptIC = IDL.Record({
+    'ledger' : IDL.Principal,
+    'account' : IDL.Opt(Account),
+  });
+  const EndpointOptOther = IDL.Record({
+    'platform' : IDL.Nat64,
+    'ledger' : IDL.Vec(IDL.Nat8),
+    'account' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const EndpointOpt = IDL.Variant({
+    'ic' : EndpointOptIC,
+    'other' : EndpointOptOther,
+  });
+  const DestinationEndpointResp = IDL.Record({
+    'endpoint' : EndpointOpt,
+    'name' : IDL.Text,
+  });
+  const EndpointIC = IDL.Record({
+    'ledger' : IDL.Principal,
+    'account' : Account,
+  });
+  const EndpointOther = IDL.Record({
+    'platform' : IDL.Nat64,
+    'ledger' : IDL.Vec(IDL.Nat8),
+    'account' : IDL.Vec(IDL.Nat8),
+  });
+  const Endpoint = IDL.Variant({ 'ic' : EndpointIC, 'other' : EndpointOther });
+  const SourceEndpointResp = IDL.Record({
+    'balance' : IDL.Nat,
+    'endpoint' : Endpoint,
+    'name' : IDL.Text,
+  });
+  const GetNodeResponse = IDL.Record({
+    'id' : LocalNodeId,
+    'created' : IDL.Nat64,
+    'active' : IDL.Bool,
+    'modified' : IDL.Nat64,
+    'controllers' : IDL.Vec(Controller),
+    'custom' : IDL.Opt(Shared),
+    'extractors' : IDL.Vec(LocalNodeId),
+    'billing' : IDL.Record({
+      'transaction_fee' : BillingTransactionFee,
+      'expires' : IDL.Opt(IDL.Nat64),
+      'current_balance' : IDL.Nat,
+      'billing_option' : IDL.Nat,
+      'account' : Account,
+      'frozen' : IDL.Bool,
+      'cost_per_day' : IDL.Nat,
+    }),
+    'destinations' : IDL.Vec(DestinationEndpointResp),
+    'sources' : IDL.Vec(SourceEndpointResp),
+    'refund' : Account,
+  });
+  const ModifyNodeResponse = IDL.Variant({
+    'ok' : GetNodeResponse,
+    'err' : IDL.Text,
+  });
+  const CreateNodeResponse = IDL.Variant({
+    'ok' : GetNodeResponse,
+    'err' : IDL.Text,
+  });
+  const TransferResponse = IDL.Variant({ 'ok' : IDL.Nat64, 'err' : IDL.Text });
+  const DeleteNodeResp = IDL.Variant({ 'ok' : IDL.Null, 'err' : IDL.Text });
+  const CommandResponse = IDL.Variant({
+    'modify_node' : ModifyNodeResponse,
+    'create_node' : CreateNodeResponse,
+    'transfer' : TransferResponse,
+    'delete_node' : DeleteNodeResp,
+  });
+  const BatchCommandResponse = IDL.Variant({
+    'ok' : IDL.Record({
+      'id' : IDL.Nat,
+      'commands' : IDL.Vec(CommandResponse),
+    }),
+    'err' : IDL.Variant({
+      'caller_not_controller' : IDL.Null,
+      'expired' : IDL.Null,
+      'other' : IDL.Text,
+      'duplicate' : IDL.Nat,
+      'invalid_signature' : IDL.Null,
+    }),
+  });
+  const GetControllerNodesRequest = IDL.Record({
+    'id' : Controller,
+    'start' : LocalNodeId,
+    'length' : IDL.Nat32,
+  });
+  const NodeShared = IDL.Record({
+    'id' : LocalNodeId,
+    'created' : IDL.Nat64,
+    'active' : IDL.Bool,
+    'modified' : IDL.Nat64,
+    'controllers' : IDL.Vec(Controller),
+    'custom' : IDL.Opt(Shared),
+    'extractors' : IDL.Vec(LocalNodeId),
+    'billing' : IDL.Record({
+      'transaction_fee' : BillingTransactionFee,
+      'expires' : IDL.Opt(IDL.Nat64),
+      'current_balance' : IDL.Nat,
+      'billing_option' : IDL.Nat,
+      'account' : Account,
+      'frozen' : IDL.Bool,
+      'cost_per_day' : IDL.Nat,
+    }),
+    'destinations' : IDL.Vec(DestinationEndpointResp),
+    'sources' : IDL.Vec(SourceEndpointResp),
+    'refund' : Account,
+  });
+  const GetNode = IDL.Variant({ 'id' : LocalNodeId, 'endpoint' : Endpoint });
+  const BillingFeeSplit = IDL.Record({
+    'platform' : IDL.Nat,
+    'author' : IDL.Nat,
+    'affiliate' : IDL.Nat,
+    'pylon' : IDL.Nat,
+  });
+  const BillingPylon = IDL.Record({
+    'operation_cost' : IDL.Nat,
+    'freezing_threshold_days' : IDL.Nat,
+    'min_create_balance' : IDL.Nat,
+    'split' : BillingFeeSplit,
+    'ledger' : IDL.Principal,
+    'platform_account' : Account,
+    'pylon_account' : Account,
+  });
+  const Billing = IDL.Record({
+    'transaction_fee' : BillingTransactionFee,
+    'cost_per_day' : IDL.Nat,
+  });
+  const Version = IDL.Variant({
+    'alpha' : IDL.Vec(IDL.Nat16),
+    'beta' : IDL.Vec(IDL.Nat16),
+    'release' : IDL.Vec(IDL.Nat16),
+  });
+  const LedgerIdx = IDL.Nat;
+  const LedgerLabel = IDL.Text;
+  const EndpointsDescription = IDL.Vec(IDL.Tuple(LedgerIdx, LedgerLabel));
+  const ModuleMeta = IDL.Record({
+    'id' : IDL.Text,
+    'create_allowed' : IDL.Bool,
+    'ledger_slots' : IDL.Vec(IDL.Text),
+    'name' : IDL.Text,
+    'billing' : IDL.Vec(Billing),
+    'description' : IDL.Text,
+    'supported_ledgers' : IDL.Vec(SupportedLedger),
+    'author' : IDL.Text,
+    'version' : Version,
+    'destinations' : EndpointsDescription,
+    'sources' : EndpointsDescription,
+    'temporary_allowed' : IDL.Bool,
+    'author_account' : Account,
+  });
+  const PylonMetaResp = IDL.Record({
+    'name' : IDL.Text,
+    'billing' : BillingPylon,
+    'supported_ledgers' : IDL.Vec(SupportedLedger),
+    'request_max_expire_sec' : IDL.Nat64,
+    'governed_by' : IDL.Text,
+    'temporary_nodes' : IDL.Record({
+      'allowed' : IDL.Bool,
+      'expire_sec' : IDL.Nat64,
+    }),
+    'modules' : IDL.Vec(ModuleMeta),
+  });
+  const VirtualBalancesRequest = IDL.Record({
+    'owner' : IDL.Principal,
+    'subaccount' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+  });
+  const VirtualBalancesResponse = IDL.Vec(IDL.Tuple(SupportedLedger, IDL.Nat));
+  const NNSVECTOR = IDL.Service({
+    'get_ledger_errors' : IDL.Func([], [IDL.Vec(IDL.Vec(IDL.Text))], ['query']),
+    'get_ledgers_info' : IDL.Func([], [IDL.Vec(LedgerInfo)], ['query']),
+    'icrc3_get_archives' : IDL.Func(
+        [GetArchivesArgs],
+        [GetArchivesResult],
+        ['query'],
+      ),
+    'icrc3_get_blocks' : IDL.Func(
+        [GetBlocksArgs],
+        [GetBlocksResult],
+        ['query'],
+      ),
+    'icrc3_get_tip_certificate' : IDL.Func(
+        [],
+        [IDL.Opt(DataCertificate)],
+        ['query'],
+      ),
+    'icrc3_supported_block_types' : IDL.Func(
+        [],
+        [IDL.Vec(BlockType)],
+        ['query'],
+      ),
+    'icrc55_command' : IDL.Func(
+        [BatchCommandRequest],
+        [BatchCommandResponse],
+        [],
+      ),
+    'icrc55_get_controller_nodes' : IDL.Func(
+        [GetControllerNodesRequest],
+        [IDL.Vec(NodeShared)],
+        ['query'],
+      ),
+    'icrc55_get_defaults' : IDL.Func([IDL.Text], [CreateRequest], ['query']),
+    'icrc55_get_nodes' : IDL.Func(
+        [IDL.Vec(GetNode)],
+        [IDL.Vec(IDL.Opt(NodeShared))],
+        ['query'],
+      ),
+    'icrc55_get_pylon_meta' : IDL.Func([], [PylonMetaResp], ['query']),
+    'icrc55_virtual_balances' : IDL.Func(
+        [VirtualBalancesRequest],
+        [VirtualBalancesResponse],
+        ['query'],
+      ),
+  });
+  return NNSVECTOR;
+};
+export const init = ({ IDL }) => { return []; };
